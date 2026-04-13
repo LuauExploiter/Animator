ca93cb3f6cacad3e03748497ce7263ca1438c19a
LuauExploiter <beshoyibrahim283@gmail.com>
2026-04-12 22:13:37 -0500

    Update extract_animation.lua

--- tools/extract_animation.lua
+++ tools/extract_animation.lua
@@ -1,1 +1,12 @@
-print("stub")
+
+local function extract(source)
+	return {
+		Id = source.Id or source.AnimationId,
+		Markers = source.Markers or {},
+		Looped = source.Looped or source.Loop or false,
+		Infinite = source.Infinite or false,
+		DontDisconnectMarkers = source.DontDisconnectMarkers or false,
+	}
+end
+
+return extract
