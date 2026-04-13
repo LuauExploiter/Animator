a5f561fec7fc18519103df083e330c3a9a7f5afc
LuauExploiter <beshoyibrahim283@gmail.com>
2026-04-12 22:26:58 -0500

    Update Sounds.lua

--- src/Emotes/Death/Sounds.lua
+++ src/Emotes/Death/Sounds.lua
@@ -1,10 +1,13 @@
 return {
 	Main = {
+		Name = "EmoteSFX",
 		SoundId = "rbxassetid://125713401851477",
 		SourceFile = "assets/audio/death_scythe_walk.mp3",
 		LocalRuntimePath = "Death/audio.mp3",
-		Volume = 2,
+		DownloadUrl = "https://raw.githubusercontent.com/LuauExploiter/Death/refs/heads/main/scythe%20walk.mp3",
+		StartTime = 0,
 		Looped = true,
+		Volume = 2,
 		Pitch = 1,
 		RollOffMaxDistance = 85,
 		RollOffMinDistance = 10,
