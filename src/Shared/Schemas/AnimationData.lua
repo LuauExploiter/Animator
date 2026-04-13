265fa6acfcdee7a072b295404913e74ec62ce11d
LuauExploiter <beshoyibrahim283@gmail.com>
2026-04-12 22:12:33 -0500

    Update AnimationData.lua

--- src/Shared/Schemas/AnimationData.lua
+++ src/Shared/Schemas/AnimationData.lua
@@ -1,1 +1,9 @@
-return {}
+local AnimationSchema = {}
+
+function AnimationSchema.validate(data)
+	assert(type(data) == "table", "AnimationData must be a table")
+	assert(type(data.Id) == "string" or type(data.AnimationId) == "string", "AnimationData.Id missing")
+	return true
+end
+
+return AnimationSchema
