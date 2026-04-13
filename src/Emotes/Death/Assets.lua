29f00db0133abb08db06062e853cbecffadeb403
LuauExploiter <beshoyibrahim283@gmail.com>
2026-04-12 20:34:54 -0500

    scaffold: add/update src/Emotes/Death/Assets.lua

---
+++ src/Emotes/Death/Assets.lua
@@ -0,0 +1,13 @@
+return {
+	Startup = {
+		{ From = "BadWolf/HandleL", To = "Character/Left Arm" },
+		{ From = "BadWolf/HandleR", To = "Character/Right Arm" },
+		{ From = "BadWolf/Left", To = "Character/HumanoidRootPart" },
+		{ From = "BadWolf/Right", To = "Character/HumanoidRootPart" },
+	},
+	Optional = {
+		{ From = "BadWolf/Sparks2", To = "Character" },
+		{ From = "BadWolf/SpinL", To = "Character" },
+		{ From = "BadWolf/SpinR", To = "Character" },
+	},
+}
