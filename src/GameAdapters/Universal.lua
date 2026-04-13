ea5b56817092ce34f3791cc68d3c0674da1c1f01
LuauExploiter <beshoyibrahim283@gmail.com>
2026-04-12 20:34:46 -0500

    scaffold: add/update src/GameAdapters/Universal.lua

---
+++ src/GameAdapters/Universal.lua
@@ -0,0 +1,15 @@
+return {
+	DashKeys = { "Q" },
+	BlockKeys = { "F", "MouseButton2" },
+	M1Keys = { "MouseButton1" },
+	DashChildren = { "InDash", "MechDash" },
+	M1Children = { "M1ing" },
+	BlockAttribute = "Blocking",
+	EmoteStartedAttribute = "EmoteStarted",
+	MobileButtons = {
+		Jump = "JumpButton",
+		Dash = "DashButton",
+		Block = "BlockButton",
+		M1 = "PunchButton",
+	},
+}
