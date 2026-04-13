c5ad9e2ddeb762f178f2d9978a2948144a85f2e8
LuauExploiter <beshoyibrahim283@gmail.com>
2026-04-13 17:19:04 -0500

    Create death.lua

---
+++ assets/animations/death.lua
@@ -0,0 +1,13 @@
+return {
+	Id = "rbxassetid://74441004296237",
+	Loop = true,
+	Looped = true,
+	Priority = Enum.AnimationPriority.Action4,
+	Markers = {
+		"stop",
+		"clang",
+		"restart",
+		"spin",
+	},
+	Frames = nil,
+}
