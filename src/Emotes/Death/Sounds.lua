879c0264c31b8fd4cc7794609e0ad2457b81762d
LuauExploiter <beshoyibrahim283@gmail.com>
2026-04-12 21:57:38 -0500

    Update Sounds.lua

--- src/Emotes/Death/Sounds.lua
+++ src/Emotes/Death/Sounds.lua
@@ -1,12 +1,10 @@
 return {
 	Main = {
-		Name = "EmoteSFX",
-		SourceType = "roblox_asset",
 		SoundId = "rbxassetid://125713401851477",
 		SourceFile = "assets/audio/death_scythe_walk.mp3",
-		StartTime = 0,
-		Looped = true,
+		LocalRuntimePath = "Death/audio.mp3",
 		Volume = 2,
+		Looped = true,
 		Pitch = 1,
 		RollOffMaxDistance = 85,
 		RollOffMinDistance = 10,
