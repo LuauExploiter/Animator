3a9c3aaa9feeae2cc54947d0bc7a743ff4f0fafa
LuauExploiter <beshoyibrahim283@gmail.com>
2026-04-12 22:47:45 -0500

    Create RunDeath.client.lua

---
+++ src/RunDeath.client.lua
@@ -0,0 +1,41 @@
+local Players = game:GetService("Players")
+
+local LocalPlayer = Players.LocalPlayer
+local Loader = require(script.Parent:WaitForChild("Loader"))
+
+local currentSession
+
+local function stopCurrent()
+	if currentSession and currentSession.Stop then
+		pcall(function()
+			currentSession:Stop("rerun")
+		end)
+	end
+	currentSession = nil
+end
+
+local function runForCharacter(character)
+	stopCurrent()
+
+	local ok, result = pcall(function()
+		return Loader.play(character)
+	end)
+
+	if ok then
+		currentSession = result
+	else
+		warn("[Death] Failed to start:", result)
+	end
+end
+
+local function onCharacterAdded(character)
+	character:WaitForChild("Humanoid")
+	character:WaitForChild("HumanoidRootPart")
+	runForCharacter(character)
+end
+
+if LocalPlayer.Character then
+	task.spawn(onCharacterAdded, LocalPlayer.Character)
+end
+
+LocalPlayer.CharacterAdded:Connect(onCharacterAdded)
