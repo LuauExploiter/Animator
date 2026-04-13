dcf035fce501707aacd20142e141c29f00c706f3
LuauExploiter <beshoyibrahim283@gmail.com>
2026-04-12 20:34:49 -0500

    scaffold: add/update src/Emotes/Death/Sounds.lua

---
+++ src/Emotes/Death/Sounds.lua
@@ -0,0 +1,16 @@
+return {
+	Main = {
+		Name = "EmoteSFX",
+		SourceType = "roblox_asset",
+		SoundId = "rbxassetid://125713401851477",
+		SourceFile = "assets/audio/death_scythe_walk.mp3",
+		StartTime = 0,
+		Looped = true,
+		Volume = 2,
+		Pitch = 1,
+		RollOffMaxDistance = 85,
+		RollOffMinDistance = 10,
+		EmitterSize = 10,
+		FadeOutTime = 0.08,
+	},
+}
